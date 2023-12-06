d517d7ccb22bd9bae949f0ebc3da83c56e36a8f8
unknown <5663314@qq.com>
2023-12-06 15:29:59 +0800

    update

--- spring-log-example/src/main/java/com/dusizhong/examples/log/component/LoginEventListener.java
+++ spring-log-example/src/main/java/com/dusizhong/examples/log/component/LoginEventListener.java
@@ -12,14 +12,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
 /**
@@ -39,35 +40,37 @@ public class LoginEventListener implements ApplicationListener<AuthenticationSuc
     public void onApplicationEvent(AuthenticationSuccessEvent authenticationSuccessEvent) {
         Authentication authentication = authenticationSuccessEvent.getAuthentication();
         Object principal = authentication.getPrincipal();
-        //String username = principal instanceof UserDetails ? ((UserDetails) principal).getUsername() : principal.toString();
-        HttpServletRequest request = ((ServletRequestAttributes) Objects.requireNonNull(RequestContextHolder.getRequestAttributes())).getRequest();
-        UserAgent userAgent = UserAgent.parseUserAgentString(request.getHeader("User-Agent"));
-        OperatingSystem os = userAgent.getOperatingSystem();
-        Browser browser = userAgent.getBrowser();
-        String ip = IpUtils.getIpAddr(request);
+        String username = principal instanceof UserDetails ? ((UserDetails) principal).getUsername() : principal.toString();
         JSONObject currentUser = (JSONObject) JSONObject.toJSON(principal);
-        //log.info("用户认证成功: {}, IP地址: {}, 浏览器信息: {}", request.getRequestURI(), ip, userAgent);
 
-        if(request.getRequestURI().endsWith("/oauth/token")) {
-            SysLog sysLog = new SysLog();
-            sysLog.setOpUrl(request.getRequestURI());
-            sysLog.setOpBiz("Oauth");
-            sysLog.setOpBizId(currentUser.getString("id"));
-            sysLog.setOpBizData(null);
-            sysLog.setOpStartTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")));
-            sysLog.setOpEndTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")));
-            sysLog.setOpBizResult(currentUser.toJSONString());
-            sysLog.setOpUserId(currentUser.getString("id"));
-            sysLog.setOpUserName(currentUser.getString("name"));
-            sysLog.setOpUserIp(ip);
-            sysLog.setOpUserOs(os.getName());
-            sysLog.setOpUserBrowser(browser.getName());
-            sysLog.setOpPlatform("LOG_SERVER");
-            sysLog.setOpTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")));
-            CompletableFuture.runAsync(() -> sysLogRepository.save(sysLog)).exceptionally(e -> {
-                log.error("异步保存日志失败: " + e.getMessage());
-                return null;
-            });
+        if(!ObjectUtils.isEmpty(RequestContextHolder.getRequestAttributes())) {
+            HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
+            String url = request.getRequestURI();
+            String ip = IpUtils.getIpAddr(request);
+            UserAgent userAgent = UserAgent.parseUserAgentString(request.getHeader("User-Agent"));
+            OperatingSystem os = userAgent.getOperatingSystem();
+            Browser browser = userAgent.getBrowser();
+            if(url.endsWith("/oauth/token")) {
+                SysLog sysLog = new SysLog();
+                sysLog.setOpUrl(request.getRequestURI());
+                sysLog.setOpBiz("Oauth");
+                sysLog.setOpBizId(currentUser.getString("id"));
+                sysLog.setOpBizData(null);
+                sysLog.setOpStartTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")));
+                sysLog.setOpEndTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")));
+                sysLog.setOpBizResult(currentUser.toJSONString());
+                sysLog.setOpUserId(currentUser.getString("id"));
+                sysLog.setOpUserName(currentUser.getString("name"));
+                sysLog.setOpUserIp(ip);
+                sysLog.setOpUserOs(os.getName());
+                sysLog.setOpUserBrowser(browser.getName());
+                sysLog.setOpPlatform("LOG_EXAMPLE");
+                sysLog.setOpTime(LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss.SSS")));
+                CompletableFuture.runAsync(() -> sysLogRepository.save(sysLog)).exceptionally(e -> {
+                    log.error("异步保存日志失败: " + e.getMessage());
+                    return null;
+                });
+            }
         }
     }
 }
